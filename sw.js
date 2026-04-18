99f8e88e90337f74a22dc76648e90078bc0ea991
mkmdp2020 <mkmdp2020@gmail.com>
2026-04-18 09:34:48 +0100

    fix: add passthrough service worker for Decap CMS

---
+++ sw.js
@@ -0,0 +1,4 @@
+// Passthrough service worker for Decap CMS
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', event => event.respondWith(fetch(event.request)));
